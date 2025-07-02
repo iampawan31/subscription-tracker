53fc8b70f7577ad059b9dd43a65925d8ceeebabd
Pawan Kumar <surpawan@gmail.com>
2025-07-03 01:30:32 +0530

    add: add model schemas

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { PORT } from './config/env.js'
+import connectToDatabase from './database/mongodb.js'
+import authRouter from './routes/auth.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import userRouter from './routes/user.routes.js'
+
+const app = express()
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/users', userRouter)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Subscription Tracker API!!')
+})
+
+app.listen(PORT, async () => {
+  console.log(`Subscription Tracker API running on http://localhost:${PORT}`)
+
+  await connectToDatabase()
+})
+
+export default app
---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'User Name is required'],
+      trim: true,
+      minLength: 2,
+      maxLength: 30
+    },
+    email: {
+      type: String,
+      required: [true, 'User Email is required'],
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    password: {
+      type: String,
+      required: [true, 'User Password is required'],
+      minLength: 6,
+      select: false // Do not return password by default
+    }
+  },
+  {
+    timestamps: true // Automatically manage createdAt and updatedAt fields
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
